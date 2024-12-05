f484854b7fab9fb9d30e2319d8c555747ac83f2f
LIN, GUAN-YU <linguanyul@gmail.com>
2024-12-05 15:10:46 +0800

    Update main2.c

--- EX3/main2.c
+++ EX3/main2.c
@@ -1,1 +1,16 @@
-
+int main(){
+    int i = 2;
+    if (i % 15 == 0) {
+        printf("Love IU");
+    }
+    else if (i % 3 == 0) {
+        printf("Love");
+    }
+    else if (i % 5 == 0) {
+        printf("IU");
+    }
+    else {
+        printf("%d\n", i);
+    }
+    return 0;
+}
