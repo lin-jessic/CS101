77d1510823c01e17d08d185200f099e946fb9281
LIN, GUAN-YU <linguanyul@gmail.com>
2024-12-05 20:44:21 +0800

    Update main3.c

--- EX5/main3.c
+++ EX5/main3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1 , j = 1 , k = 1; k <= 81; k++) {
-        printf("%d * %d = %d\t", i, j, i * j);
+    for (int i = 1 , j = 1 , k = 1 ; k <= 81 ; k++) {
+        printf("%d * %d = %d\t", i , j , i * j);
         j++;
         if (j > 9) {
             j = 1;
