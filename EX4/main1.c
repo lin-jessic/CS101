e27a81bd05f333d69c6a69e7d38e2e6c86e6c309
LIN, GUAN-YU <linguanyul@gmail.com>
2024-12-12 13:28:15 +0800

    Update main1.c

--- EX4/main1.c
+++ EX4/main1.c
@@ -1,17 +1,1 @@
-#include <stdio.h> //自己的版本
 
-int main(){
-    int n = 7;
-    for (int i = 1 ; i <= n ; i++) {
-        for (int j = 1 ; j <= n + i - 1 ; j++) {
-            if ( j <= n-i ) {
-                printf(" ");
-            }
-            else {
-                printf("%d" , i);
-            }
-        }
-        printf("\n");
-    }
-    return 0;
-}
